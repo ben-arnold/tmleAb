5ef46acefcbaea48f8cd4c5724741e5aaf4cb015
ben-arnold <benarnold@berkeley.edu>
2016-09-05 09:28:06 -0700

    Updated package metadata

--- R/startup.R
+++ R/startup.R
@@ -1,7 +1,7 @@
 
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to the tmleAb package\nTargeted maximum likelihood estimation for antibody (Ab) measurements.")
+  packageStartupMessage("Welcome to the tmleAb package\nTargeted maximum likelihood estimation for antibody measurements.\n\nVersion 0.1.0.\n\nCheck github.com/ben-arnold/tmleAb for the most current version -- we have planned some useful additions!")
 }
 
 .onLoad <- function(libname, pkgname) {
@@ -11,7 +11,7 @@
     devtools.install.args = "",
     devtools.name = "Ben Arnold",
     devtools.desc.author = '"Ben Arnold <benarnold@berkeley.edu> [aut, cre]"',
-    devtools.desc.license = "Creative Commons Attribution 3.0 Unported License",
+    devtools.desc.license = "GNU General Public License v3.0",
     devtools.desc.suggests = NULL,
     devtools.desc = list()
   )
